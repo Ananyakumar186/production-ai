ecd0439140363334b6b52f63d5834bfce241c5d6
Ananya Kumar <ananyakumar565@gmail.com>
2025-11-28 13:03:51 +0530

    removing openai api key

--- projects/week-2/twin/terraform/main.tf
+++ projects/week-2/twin/terraform/main.tf
@@ -136,7 +136,6 @@ resource "aws_lambda_function" "api" {
       CORS_ORIGINS     = var.use_custom_domain ? "https://${var.root_domain},https://www.${var.root_domain}" : "https://${aws_cloudfront_distribution.main.domain_name}"
       S3_BUCKET        = aws_s3_bucket.memory.id
       USE_S3           = "true"
-      OPENAI_API_KEY   = var.openai_api_key
     }
   }
 
